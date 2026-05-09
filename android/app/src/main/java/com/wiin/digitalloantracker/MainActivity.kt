244f7a5c9ebeae435184c4e36fc20a76f13e887a
hammadkhan2930029 <hammad2930029@gmail.com>
2026-05-09 12:41:52 -0700

    updated

--- android/app/src/main/java/com/wiin/digitalloantracker/MainActivity.kt
+++ android/app/src/main/java/com/wiin/digitalloantracker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.digitalloantracker
+package com.wiin.digitalloantracker
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/wiin/digitalloantracker/MainApplication.kt
+++ android/app/src/main/java/com/wiin/digitalloantracker/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.digitalloantracker
+package com.wiin.digitalloantracker
 
 import android.app.Application
 import com.facebook.react.PackageList
